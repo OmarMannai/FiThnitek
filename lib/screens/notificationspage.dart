f28db07437b86b3cbfd008dac71fc3c076489d6e
omar mannai <mannaiomar28@gmail.com>
2023-07-27 13:39:26 +0100

    dismissable notifications | livreur details page

--- lib/screens/notificationspage.dart
+++ lib/screens/notificationspage.dart
@@ -62,7 +62,7 @@ class _NotificationsPageState extends State<NotificationsPage> {
                   physics: NeverScrollableScrollPhysics(),
                   itemCount: notifications.length,
                   itemBuilder: (context, index) {
-                    final notification=notifications[index];
+                    final notification = notifications[index];
                     return Dismissible(
                       direction: DismissDirection.startToEnd,
                       key: Key(index.toString()),
@@ -134,44 +134,56 @@ class _NotificationsPageState extends State<NotificationsPage> {
                   physics: NeverScrollableScrollPhysics(),
                   itemCount: notifications.length,
                   itemBuilder: (context, index) {
-                    return Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(25),
-                            child: Image.network(
-                              'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-                              fit: BoxFit.cover,
+                    final notification = notifications[index];
+                    return Dismissible(
+                      direction: DismissDirection.startToEnd,
+                      key: Key(index.toString()),
+                      background: Container(
+                        padding: EdgeInsets.only(left: 10),
+                        alignment: Alignment.centerLeft,
+                        color: Colors.grey,
+                        child: Icon(Icons.delete),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(25),
+                              child: Image.network(
+                                'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              notifications[index].fullName,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xff565656),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                notification.fullName,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff565656),
+                                ),
                               ),
-                            ),
-                            Text(
-                              'delivery persons close to you ',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xff939796),
+                              Text(
+                                'delivery persons close to you',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff939796),
+                                ),
                               ),
-                            ),
-                          ],
-                        )
-                      ],
+                            ],
+                          )
+                        ],
+                      ),
+                      onDismissed: (direction) => notifications.removeAt(index),
                     );
                   },
                   separatorBuilder: (context, index) {
@@ -194,44 +206,56 @@ class _NotificationsPageState extends State<NotificationsPage> {
                   physics: NeverScrollableScrollPhysics(),
                   itemCount: notifications.length,
                   itemBuilder: (context, index) {
-                    return Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(25),
-                            child: Image.network(
-                              'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-                              fit: BoxFit.cover,
+                    final notification = notifications[index];
+                    return Dismissible(
+                      direction: DismissDirection.startToEnd,
+                      key: Key(index.toString()),
+                      background: Container(
+                        padding: EdgeInsets.only(left: 10),
+                        alignment: Alignment.centerLeft,
+                        color: Colors.grey,
+                        child: Icon(Icons.delete),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(25),
+                              child: Image.network(
+                                'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              notifications[index].fullName,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xff565656),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                notification.fullName,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff565656),
+                                ),
                               ),
-                            ),
-                            Text(
-                              'delivery persons close to you ',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xff939796),
+                              Text(
+                                'delivery persons close to you',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff939796),
+                                ),
                               ),
-                            ),
-                          ],
-                        )
-                      ],
+                            ],
+                          )
+                        ],
+                      ),
+                      onDismissed: (direction) => notifications.removeAt(index),
                     );
                   },
                   separatorBuilder: (context, index) {
